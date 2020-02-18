2748a5850174d9983f0a9ca5856520dc82ddf553
Adesh Nalpet <adesh.nalpet@phonpe.com>
2020-02-18 10:32:59 +0000

    add code in files types: .xml,.java,.yml remove code in files types: .xml,.java,.yml

--- content.java
+++ content.java
@@ -104,6 +104,6 @@ public class Cdcist {
         System.out.println("fpmxp");
         System.out.println("tpsas");
         System.out.println("ikego");
-        System.out.println("rhdvk");
+        System.out.println("yiiam");
     }
 }
