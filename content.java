718cafc6ad08e5f321ffa05f4a25c473ddddae4d
Adesh Nalpet <adesh.nalpet@PP-C02Z66CALVCG.local>
2020-02-24 09:47:01 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -17,6 +17,6 @@ public class Cdcist {
         System.out.println("wcuwc");
         System.out.println("sflmt");
         System.out.println("kvsgp");
-        System.out.println("hdjjg");
+        System.out.println("khjrh");
     }
 }
