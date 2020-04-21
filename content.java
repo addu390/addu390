abce25274ae36d6f1d282be4fa6c9be1472d5cda
Adesh Nalpet <adesh.nalpet@PP-C02Z66CALVCG.local>
2020-04-21 07:42:10 +0000

    add code in files types: .java,.yml remove code in files types: .java,.yml

--- content.java
+++ content.java
@@ -22,6 +22,6 @@ public class Cdcist {
         System.out.println("cepcm");
         System.out.println("khdpy");
         System.out.println("rraao");
-        System.out.println("xtbqf");
+        System.out.println("bxaas");
     }
 }
